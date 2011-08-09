12367de3b74f7d62943a318caf0408d6cd07a776
donald.becker <donald.becker@gmail.com>
2011-08-09 09:15:59 +0000

    Initial version of PIC-specific device support.

--- obd2-instruments/PIC18/pic-devs.c
+++ obd2-instruments/PIC18/pic-devs.c
@@ -22,6 +22,14 @@
 static const char versionA[] =
 "$Id: pic-devs.c $\n";
 
+#include <stdint.h>
+#include <pic18fregs.h>
+
+#include "vvvvroom.h"
+#include "can.h"
+#include "command.h"
+
+
 /* Watchdog section.  Ready for future functionality. */
 void wdt_reset(void)
 {
@@ -38,8 +46,35 @@ void watchdog_enable(void)
  * This changes with the hardware platform and specific MCU part.
  * We need to set up the PWM timer, clock timer, ADC, LED pins,
  * SPI, USART and CAN.
+ */
 void setup_io_ports(void)
 {
+	/* Select the external OSC1 8MHz crystal. */
+	OSCCON = 0x74;
+	/* Set outputs low before setting the direction. */
+	PORTA = 0;
+	PORTB = 0;
+	PORTC = 0;
+	SSPCON1bits.SSPEN = 0;		/* Disable serial port */
+	TRISA = 0xFF;				/* Analog inputs */
+	TRISB = 0xFF;				/* Set all to inputs */
+	TRISC = 0x8F;				/* Serial port (B7,B6). */
+	/* Set the ADC to Vdd/Vss reference, AN0..AN5 as analog inputs. */
+	ADCON1 = 0x0A;
+
+	/* Initialize the USART */
+	BAUDCON = 0;				/* Async 8 bit mode. */
+	SPBRG = 0x19;
+	TXSTA = 0xA4;
+	RCSTA = 0x10;				/* Leave reset. */
+	PIR2 = PIR1 = 0;
+	PIE1 = 0x20;
+	PIE2 = 0;
+	/* Setup interrupts. */
+	INTCON = 0xE0;				/* Enable intrs: peripherals and timer0 */
+	RCONbits.IPEN = 1;			/* Set interrupt priority mode */
+	IPR1bits.TXBIP = 1;			/* Set UART as a high priority interrupt. */
+	INTCON2bits.TMR0IP = 0;		/* Set timer0 as a low priority interrupt. */
 }
 
 /*
@@ -47,6 +82,10 @@ void setup_io_ports(void)
  */
 void setup_timers(void)
 {
+	T0CON = 5;
+	/* Set timer period to 34285 = 0x85ED. */
+	TMR0L = 0xED;
+	TMR0H = 0x85;
 }
 
 /*
